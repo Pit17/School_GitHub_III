77bafae39b985e4f5eb2c294da095f5367df0c65
Pietro Malzone <malzonepietro3@gmail.com>
2023-10-10 12:56:25 +0200

    Aggiornamento lavori 10/10/2023
    Comprende compiti per info 11/10/2023

---
+++ LavoriL10(Prof. Molara)/ConsoleApp1LavoroIf/ConsoleApp1LavoroIf/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1LavoroIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double soglia1 = 0.15;
+            double soglia2 = 0.2;
+            double redditoLordo, redditoNetto,imposta;
+
+            Console.Write("\nPerfavore inserisca il proprio reddito lordo --> ");
+            redditoLordo = Double.Parse(Console.ReadLine());
+
+            if (redditoLordo > 10000)
+            {
+                imposta = ((redditoLordo - 10000) * soglia2) + (10000 * soglia1);
+                redditoNetto = redditoLordo - imposta;
+            }
+            else
+            {
+                imposta = (redditoLordo * soglia1);
+                redditoNetto = redditoLordo - imposta;
+            }
+
+            Console.WriteLine($"\n Le sono stai sottratti dallo Stato {imposta} euro perciò il suo reddito netto è {redditoNetto} euro");
+
+        }
+
+
+    }
+}
