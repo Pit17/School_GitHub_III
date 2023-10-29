2d6a2be05422a2e122814d32e848c8b5a0c8f6e8
Pit17 <malzonepietro3@gmail.com>
2023-10-29 13:44:01 +0100

    Update
    Validazione Data terminato (compito Informatica)

--- Compiti Molara/ConsoleAppValidazioneDiUnaData/ConsoleAppValidazioneDiUnaData/Program.cs
+++ Compiti Molara/ConsoleAppValidazioneDiUnaData/ConsoleAppValidazioneDiUnaData/Program.cs
@@ -1,34 +1,12 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ConsoleAppValidazioneDiUnaData
 {
     internal class Program
 
     {
-        static int ValidazioneData()
-        {
-            bool validazione = false;
-            int anno = InputDati("Inserisca l'anno ---> ",0,int.MaxValue);
-            bool bisestile = Bisestile(anno);
-            int mese = InputDati("Inserisca il mese -->", 0, 13);
-            int giorno = InputDati("Insersica il giorno --> ", 0, 32);
-            if (mese == 11 || mese == 9 || mese == 6 || mese = 4)
-            {
-                if (giorno <= 30) validazione = true;
-            }
-            if (mese == 2) 
-            { 
-               if (bisestile == true)
-                {
-                    if(giorno <= 29) validazione = true;
-                }
-               else
-                {
-                    if (giorno <= 28) validazione = true;
-                }
-            }
-        
-        }
+
         static bool Bisestile(int anno) 
         {
             bool bisestile; 
@@ -46,24 +24,65 @@ namespace ConsoleAppValidazioneDiUnaData
         static int InputDati(string messaggio, int valoreMin, int valoreMax)
         {
             int valore;
-            while (true)
+            bool InputOk;
+
+            do
             {
                 Console.Write(messaggio);
-                if (int.TryParse(Console.ReadLine(), out valore))
-                    continue;
+                InputOk = int.TryParse(Console.ReadLine(), out valore);
+                if (!InputOk || valore < valoreMin || valore > valoreMax)
+                {
+                    Console.WriteLine("Errore! Inserimento non valido");
+                    InputOk = false;
+                }
+            } while (!InputOk);
 
-                if (valore < valoreMin || valore > valoreMax)
-                    continue;
-
-
-                break;
-            }
             return valore;
-
          }
+
+        static bool ValidazioneData()
+        {
+            bool validazione = false;
+            int anno = InputDati("Inserisca l'anno ---> ", 0, int.MaxValue);
+            bool bisestile = Bisestile(anno);
+            int mese = InputDati("Inserisca il mese -->", 0, 13);
+            int giorno = InputDati("Inserisca il giorno --> ", 0, 32);
+            switch (mese)
+            {
+                case 1 or 3 or 5 or 7 or 8 or 10 or 12:
+                    if (giorno <= 31) validazione = true;
+                    else validazione = false;
+                    break;
+                    
+
+                case 4 or 6 or 9 or 11:
+                    if (giorno <= 30) validazione = true;
+                    else validazione = false;
+                    break;
+
+                case 2:
+                    if (bisestile)
+                    {
+                        if (giorno <= 29) validazione = true;
+                        else validazione = false;
+                    }
+                    else
+                    {
+                        if (giorno <= 28) validazione = true;
+                        else validazione = false;
+                    }
+                    break;
+                
+            }
+           return validazione;
+        }
+
         static void Main(string[] args)
         {
-          
+          bool validazione = ValidazioneData();
+          if (validazione) Console.WriteLine("Data valida");
+          else Console.WriteLine("Data non valida");
+          Console.ReadKey();
         }
     }
 }
