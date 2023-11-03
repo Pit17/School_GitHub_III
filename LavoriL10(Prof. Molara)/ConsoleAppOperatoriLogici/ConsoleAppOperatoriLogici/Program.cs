572cfc3ec110583cf30089fb6ef74c2bd3751849
Pietro Malzone <pietro.malzone.stud@ispascalcomandini.it>
2023-11-03 10:53:40 +0100

    Operatori logici

---
+++ LavoriL10(Prof. Molara)/ConsoleAppOperatoriLogici/ConsoleAppOperatoriLogici/Program.cs
@@ -0,0 +1,147 @@
+﻿namespace ConsoleAppOperatoriLogici
+{
+    internal class Program
+    {
+
+
+        static bool AND()
+        {
+            bool A = true, B = true, C = true;
+            for (int i = 1; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        A = false;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}    | {A && B}");
+                        break;
+
+                    case 2:
+                        A = false;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}   | {A && B}");
+                        break;
+
+                    case 3:
+                        A = true;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}   | {A && B}");
+                        break;
+
+                    case 4:
+                        A = true;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}   | {A && B}");
+                        break;
+                }
+
+            }
+            return C;
+        }
+
+        static bool OR()
+        {
+
+            bool A = true, B = true, C = true;
+            for (int i = 1; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        A = false;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}  | {A || B}");
+                        break;
+
+                    case 2:
+                        A = false;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}   | {A || B}");
+                        break;
+
+                    case 3:
+                        A = true;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}  | {A || B}");
+                        break;
+
+                    case 4:
+                        A = true;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}   | {A || B}");
+                        break;
+                }
+
+            }
+            return C;
+        }
+
+        static bool XOR()
+        {
+
+            bool A = true, B = true, C = true;
+            for (int i = 1; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        A = false;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}  | {A != B}");
+                        break;
+
+                    case 2:
+                        A = false;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}  | {A != B}");
+                        break;
+
+                    case 3:
+                        A = true;
+                        B = false;
+                        Console.WriteLine($"{A}  | {B}   | {A != B}");
+                        break;
+
+                    case 4:
+                        A = true;
+                        B = true;
+                        Console.WriteLine($"{A}  | {B}   | {A != B}");
+                        break;
+                }
+
+            }
+            return C;
+        }
+
+
+        static bool NOT()
+        {
+            bool A = true, C = true;
+            for (int i = 1; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        A = false;
+                        Console.WriteLine($"{A}  | {!A}");
+                        break;
+
+                    case 2:
+                        A = true;
+                        Console.WriteLine($"{A} | {!A}");
+                        break;
+
+                }
+            }
+            return C;
+        }
+        static void Main(string[] args)
+        {
+            AND();
+            OR();
+            XOR();
+            NOT();
+        }
+    }
+}
