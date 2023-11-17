1c3a46a172aa6781e1ecabbcfaa6996dab8a5ebe
Pietro Malzone <malzonepietro3@gmail.com>
2023-11-17 10:56:45 +0100

    Aggiunta lavori laboratorio L10

---
+++ LavoriL10(Prof. Molara)/ConsoleAppRicercaLineare/ConsoleAppRicercaLineare/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleAppRicercaLineare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string[] elenco = { "Marco", "Lucia", "Giorgio", "Pamela", "Antonio", "Carla", "Fabio" };
+            Console.Write("\nInserisca il nome che desidera verificare -->");
+            string nome = Console.ReadLine();
+            bool flag = false;
+            int i;
+
+            for (i = 0; i < elenco.Length; i++)
+            {
+                if (elenco[i].ToUpper() == nome.ToUpper())
+                {
+                    flag = true;
+                    break;
+                }
+                
+            }
+            if (flag == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Il nome {nome} è alla posizione: {i + 1}");
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Il nome {nome} non è in elenco.");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
