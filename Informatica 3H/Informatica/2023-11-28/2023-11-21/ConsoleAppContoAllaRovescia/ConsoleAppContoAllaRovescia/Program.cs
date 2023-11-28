e001deb2c80685589492cbedd2fa374f7226f378
Pietro Malzone <malzonepietro3@gmail.com>
2023-11-28 10:13:33 +0100

    Aggiunta e riassunto lavori svolti in assenza del prof

---
+++ Informatica 3H/Informatica/2023-11-28/2023-11-21/ConsoleAppContoAllaRovescia/ConsoleAppContoAllaRovescia/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Autore Pietro Malzone 3H Numeri 7 digit dato un numero lo stampa a video
+namespace ConteggioAllaRovescia
+{
+    internal class Program
+    {
+        static string[] cifra_0 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+        static string[] cifra_1 = {
+            "       ▓",
+            "       ▓",
+            "       ▓",
+            "        ",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+        };
+
+        static string[] cifra_2 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+            "▓       ",
+            "▓       ",
+            " ▓▓▓▓▓▓ ",
+        };
+        static string[] cifra_3 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            "   ▓▓▓▓▓",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+        static string[] cifra_4 = {
+            "▓      ▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            "       ▓ ",
+        };
+        static string[] cifra_5 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ",
+            "▓      ",
+            " ▓▓▓▓▓▓",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+        static string[] cifra_6 = {
+            " ▓▓▓▓▓▓ ",
+            "▓       ",
+            "▓       ",
+            " ▓▓▓▓▓▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+        static string[] cifra_7 = {
+            " ▓▓▓▓▓▓ ",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+            "       ▓",
+            "       ▓ ",
+        };
+        static string[] cifra_8 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓ ",
+
+        };
+        static string[] cifra_9 = {
+            " ▓▓▓▓▓▓ ",
+            "▓      ▓",
+            "▓      ▓",
+            " ▓▓▓▓▓▓▓",
+            "       ▓",
+            "       ▓",
+            " ▓▓▓▓▓▓ ",
+        };
+
+
+
+
+
+        static void StampaCifra(string[] cifra,int riga ,int colonna)
+        {
+                Console.SetCursorPosition(colonna, riga);
+            foreach (string s in cifra)
+            {
+                Console.Write(s);
+            }
+                
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "PietroMalzone3H";
+            bool inputOK = true;
+            int number;
+            string numero;
+            string[][] nums = { cifra_0, cifra_1, cifra_2, cifra_3, cifra_4, cifra_5, cifra_6, cifra_7, cifra_8, cifra_9 };
+            Console.Write("Scrivere il numero che si desidera stampare: ");
+            while (true) 
+            { 
+                numero = Console.ReadLine();
+                inputOK = int.TryParse(numero, out number);
+                if (inputOK == true) break;
+                else Console.WriteLine("Numero non valido.");
+            
+            }
+            
+            for (int j = 0; j < 7; j++)//stampa riga per riga di tutti i  numeri contemporaneamente.
+            {
+                for (int i = 0; i < numero.Length; i++)
+                {
+                    Console.Write(nums[numero[i] - 48][j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Informatica 3H/Informatica/2023-11-28/2023-11-21/ConsoleAppPianoforte/ConsoleAppPianoforte/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+//Autore Pietro Malzone 3H Pianoforte
+//Nel file .txt si possono inserire le lettere della tastiera corrispondenti alla nota desiderata.
+namespace Pianoforte
+{
+    internal class Program
+    {
+      
+        // Indice -->               0    1    2    3    4    5    6
+        // Nota -->                 DO  DO#   RE   RE#   MI   FA  FA# SOL  SOL# LA   LA#   SI  DO   RE
+        static char[] keyboard = { 'a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y' ,'h', 'u' ,'j', 'k', 'l'};
+        static int[] sound_freq = { 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 587};
+
+        static char LeggiTasto()//lettura char
+        {
+            if (!Console.KeyAvailable) return '\0';
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            return key.KeyChar;
+        }
+
+        static int TrovaFrequenza(char nota)//verifica la frequenza da segnare
+        {
+            for (int i = 0; i < keyboard.Length; i++)
+            {
+                if (keyboard[i] == nota) return sound_freq[i];
+            }
+            return 0;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "PietroMalzone3H";
+
+            Console.Write(" ____________________________________\r\n|\\                                    \\\r\n| \\                                    \\\r\n|  \\____________________________________\\\r\n|  |       __---_ _---__                |\r\n|  |      |======|=====|                |\r\n|  |      |======|=====|                |\r\n|  |  ____|__---_|_---_|______________  |\r\n|  | |                                | |\r\n|   \\ \\                                \\ \\\r\n|  \\ ||\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\| | \r\n|  ||| |                               || |\r\n \\ ||| |           -  -                || |\r\n  \\'|| |-----------\\\\-\\\\---------------|| |\r\n    \\|_|            \"  \"               \\|_|\r\n\r\n--------------------Pietro Malzone feat. Antonio De Rosa----------------------------\r\n");
+
+            Console.WriteLine("Benvenuto al SuonaTu!\nPremi '1' se vuoi lasciarci il comando! :0\nPremi '2' se ti piace suonare ;)");
+            
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case '1':
+                    leggiFile(@"..\..\file.txt");
+                    break;
+                case '2':
+                    while (true)
+                    {
+                        char tasto = LeggiTasto();
+                        if (tasto != '\0')
+                        {
+                            RiproduciFrequenza(tasto);
+                        }
+                    }
+            }
+        }
+
+        static void RiproduciFrequenza(char tasto)//suona
+        {
+                int frequenza = TrovaFrequenza(tasto);
+                if (frequenza != 0)
+                    Console.Beep(TrovaFrequenza(tasto), 400);
+        }
+        static void leggiFile(string nome)//legge da file
+        {
+            using (StreamReader sr = new StreamReader(nome))
+            {
+                while (!sr.EndOfStream)
+                {
+                    foreach (char c in sr.ReadLine()) RiproduciFrequenza(c);
+                }
+            }
+        }
+    }
+}
