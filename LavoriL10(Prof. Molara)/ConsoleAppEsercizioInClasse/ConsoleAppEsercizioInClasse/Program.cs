1707bbdebcf106ffc4667024523db54d5482ad10
Pietro Malzone <malzonepietro3@gmail.com>
2023-11-30 15:50:10 +0100

    Programma Compiti

--- LavoriL10(Prof. Molara)/ConsoleAppEsercizioInClasse/ConsoleAppEsercizioInClasse/Program.cs
+++ LavoriL10(Prof. Molara)/ConsoleAppEsercizioInClasse/ConsoleAppEsercizioInClasse/Program.cs
@@ -7,7 +7,7 @@
             
             while (true)
             {
-                Console.WriteLine("Inserisca N:");
+                Console.Write("Inserisca N:");
                 if (int.TryParse(Console.ReadLine(), out int n))
                 {
                     if (n > 0) return n;
@@ -18,15 +18,29 @@
         }
         static void Main(string[] args)
         {
-            //        Dato n > 0 intero produrre a console il seguente 
-            //        "albero binario natalizio"
-            //            n = 4 (numero di livelli) 
-            //            0
-            //       1          2
-            //     3   4     5     6      n * 2 +1 sx  / n * 2 +2 dx
-            //    7 8 9 10 11 12 13 14
 
             int n = ValidazioneInput();
+            int k = 0;
+            int giri = 1, contatore = 0;
+
+            for (int i = 1; i < Math.Pow(2,n); i++)
+            {
+                for (int j = 0; j < Math.Pow(2,n - contatore) -1; j++) Console.Write(" ");
+
+                Console.Write(i-1);
+
+                for (int j = 0; j < Math.Pow(2, n - contatore) - 1; j++) Console.Write(" ");
+
+
+                if (giri == i)
+                {
+                    contatore++;
+                    Console.WriteLine();
+                    giri = giri * 2 + 1;
+                }
+                
+
+            }
 
         }
     }
