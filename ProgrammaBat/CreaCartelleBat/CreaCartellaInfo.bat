b1cc6ca7753d86135f5e4b497ba741d042b2da7e
Pietro Malzone <pietro@laptop.fritz.box>
2023-12-01 20:31:37 +0100

    ProgrammiCreaCartelle

---
+++ ProgrammaBat/CreaCartelleBat/CreaCartellaInfo.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Imposta il percorso desiderato
+set "percorso=Z:\Informatica"
+
+
+REM Ottieni la data corrente nel formato AAAA-MM-GG usando PowerShell
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd'"') do set "data=%%a"
+
+REM Crea la cartella
+set "cartella=%percorso%\%data%"
+powershell -Command "New-Item -ItemType Directory -Path '%cartella%'"
+
+echo Cartella creata: %cartella%
+
+endlocal
